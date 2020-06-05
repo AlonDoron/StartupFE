bc2bac391094dbe2d409406480fa5d293d94ef4d
AlonDoron <alon121211@gmail.com>
2020-06-05 09:15:11 +0300

    Base Expo project init

--- App.js
+++ App.js
@@ -1,93 +1,19 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
-import {
-  LandPageScreen,
-  PhoneNumberValidationTest,
-  RegistrationScreen,
-  LoginViaPhoneScreen,
-  PhoneNumberValidation,
-} from './src/screens';
-
-import {createDrawerNavigator} from '@react-navigation/drawer';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
-
-const Drawer = createDrawerNavigator();
-const Stack = createStackNavigator();
-const MaterialBottomTabs = createMaterialBottomTabNavigator();
-
-const App = () => {
-  const createHomeStack = () => {
-    const headerOption = {
-      title: '',
-      headerTintColor: '#000',
-      headerTitleAlign: 'center',
-      headerStatusBarHeight: 0,
-      headerTransparent: true,
-      headerStyle: {
-        backgroundColor: 'none',
-      },
-    };
-    return (
-      <Stack.Navigator>
-        <Stack.Screen
-          name="LandPage"
-          component={LandPageScreen}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="LoginViaPhoneScreen"
-          component={LoginViaPhoneScreen}
-          options={{...headerOption, title: 'Login'}}
-        />
-
-        <Stack.Screen
-          name="PhoneNumberValidationTest"
-          component={PhoneNumberValidationTest}
-          options={{headerShown: false}}
-        />
-
-        <Stack.Screen
-          name="PhoneNumberValidation"
-          component={PhoneNumberValidation}
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="RegistrationScreen"
-          component={RegistrationScreen}
-          options={{...headerOption, title: 'Registration'}}
-        />
-      </Stack.Navigator>
-    );
-  };
-
+export default function App() {
   return (
-    <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name="LandPage" component={createHomeStack} />
-        <Drawer.Screen
-          name="PhoneNumberValidationTest"
-          component={PhoneNumberValidationTest}
-        />
-
-        <Drawer.Screen
-          name="LoginViaPhoneScreen"
-          component={LoginViaPhoneScreen}
-        />
-
-        <Drawer.Screen
-          name="RegistrationScreen"
-          component={RegistrationScreen}
-        />
-      </Drawer.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
   );
-};
+}
 
-const styles = StyleSheet.create({});
-
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
