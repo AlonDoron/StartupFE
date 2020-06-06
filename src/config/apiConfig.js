032cc61763d0c9efe60ba634c9bb79cc6587c500
AlonDoron <alon121211@gmail.com>
2020-06-06 17:25:08 +0300

    Added ApiConfig data

---
+++ src/config/apiConfig.js
@@ -0,0 +1,12 @@
+const SERVER_URL = "https://18.184.165.20:443/api/";
+
+const HEADERS = {
+  "Content-Type": "application/json",
+};
+
+let apiConfig = {
+  SERVER_URL,
+  HEADERS,
+};
+
+export default apiConfig;
