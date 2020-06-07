84e088d4333478821b8a3a3a8bbb59ea20794c6d
AlonDoron <alon121211@gmail.com>
2020-06-07 10:36:33 +0300

    Update apiConfig.js
    Added const ports for Login, Identity, and Signup

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,4 +1,7 @@
-const SERVER_URL = "https://18.184.165.20:443/api";
+const SERVER_URL = "https://18.184.165.20";
+const IDENTITY_PORT = "447";
+const REGISTRATION_PORT = "444";
+const LOGIN_PORT = "443";
 
 const HEADERS = {
   "Content-Type": "application/json",
@@ -6,6 +9,9 @@ const HEADERS = {
 
 let apiConfig = {
   SERVER_URL,
+  IDENTITY_PORT,
+  REGISTRATION_PORT,
+  LOGIN_PORT,
   HEADERS,
 };
 
