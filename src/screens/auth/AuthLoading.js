abc2ad2f6f7c57fc288fc9a2fcad5495de96e0ad
AlonDoron <alon121211@gmail.com>
2020-06-15 23:11:16 +0300

    Updated AuthLoading to work with the new HttpClient

--- src/screens/auth/AuthLoading.js
+++ src/screens/auth/AuthLoading.js
@@ -1,39 +1,22 @@
 import React, { useEffect } from "react";
 import { ActivityIndicator, AsyncStorage, StatusBar, View } from "react-native";
 import { DeviceConfig, ApiConfig } from "../../config";
+import HttpClient from "../../api/HttpClient";
 
 const AuthLoading = (props) => {
   useEffect(() => {
-    bootstrapAsync().then((token) =>
-      /*console.log(
-          `${ApiConfig.SERVER_URL}:${ApiConfig.IDENTITY_PORT}/api/Identity?userId=${token}`
-        )
-        */
-      /*
-        console.log({
-          ...ApiConfig.HEADERS,
-          ApiKey: "Token 97a74c03004e7d6b0658dfdfde34fd6aa4b14ddb",
-        })
-*/
-      fetch(
-        `${ApiConfig.SERVER_URL}:${ApiConfig.IDENTITY_PORT}/api/Identity?userId=${token}`,
-        {
-          headers: {
-            ...ApiConfig.HEADERS,
-            ApiKey: "97a74c03004e7d6b0658dfdfde34fd6aa4b14ddb",
-          },
-        }
-      )
-        .then((response) => response.json())
+    bootstrapAsync().then((token) => {
+      let params = {
+        userId: token,
+      };
 
+      HttpClient.get(ApiConfig.IDENTITY_PORT, "api/identity", params)
         .then((result) => {
+          console.log(result);
           props.navigation.navigate(result ? "App" : "Auth");
         })
-        .catch((error) => {
-          // console.log(error);
-          props.navigation.navigate("Auth");
-        })
-    );
+        .catch((err) => console.log(err));
+    });
   }, []);
 
   const bootstrapAsync = async () => {
