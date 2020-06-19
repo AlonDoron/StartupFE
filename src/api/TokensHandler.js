c38109d01b9ddb7963403d3da9828e5c06487f2b
AlonDoron <alon121211@gmail.com>
2020-06-19 22:20:44 +0300

    Added getTokenFromDevice function to TokensHandler

--- src/api/TokensHandler.js
+++ src/api/TokensHandler.js
@@ -5,11 +5,16 @@ const writeTokenToDevice = async (token) => {
   await AsyncStorage.setItem(DeviceConfig.DEVICE_TOKEN_NAME, token);
 };
 
-const removeTokenFromDevice = async (token) => {
+const getTokenFromDevice = async () => {
+  const token = await AsyncStorage.getItem(DeviceConfig.DEVICE_TOKEN_NAME);
+  return token;
+};
+const removeTokenFromDevice = async () => {
   await AsyncStorage.removeItem(DeviceConfig.DEVICE_TOKEN_NAME);
 };
 
 export default {
   writeTokenToDevice,
+  getTokenFromDevice,
   removeTokenFromDevice,
 };
