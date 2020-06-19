e50b1604c0d2f74941a227ecacabc8224791844a
AlonDoron <alon121211@gmail.com>
2020-06-19 13:49:31 +0300

    Updated the AuthStack to contain the new VerifyLogin screen and updated Login to navigate to it if the phone number exists.

--- src/screens/auth/Login.js
+++ src/screens/auth/Login.js
@@ -1,11 +1,23 @@
 import React, { useEffect } from "react";
 import { View, Text, AsyncStorage, Alert } from "react-native";
 import LoginForm from "../../forms/LoginForm";
+import HttpClient from "../../api/HttpClient";
+import apiConfig from "../../config/apiConfig";
 
 const Login = (props) => {
+  const handleSubmitForm = (vals) => {
+    HttpClient.get(
+      apiConfig.LOGIN_PORT,
+      "api/Login/checkIfUserExists",
+      vals
+    ).then((result) => {
+      props.navigation.navigate(result ? "VerifyLogin" : "Signup");
+    });
+  };
+
   return (
     <View>
-      <LoginForm submitForm={(vals) => console.log(vals)} />
+      <LoginForm submitForm={handleSubmitForm} />
     </View>
   );
 };
