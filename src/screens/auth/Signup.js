0044904cf7f62c35716f19d7859ff2e59efaf289
aviram5 <60235058+aviram5@users.noreply.github.com>
2020-06-21 13:29:56 +0300

    adding Signup

--- src/screens/auth/Signup.js
+++ src/screens/auth/Signup.js
@@ -1,10 +1,49 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View } from "react-native";
+import SignupForm from "../../forms/SignupForm";
+import HttpClient from "../../api/HttpClient";
+import apiConfig from "../../config/apiConfig";
 
-const Signup = () => {
+const Signup = (props) => {
+  const [submitting, setSubmitting] = useState(false);
+
+  const handleSubmitForm = (vals) => {
+    setSubmitting(true);
+
+    HttpClient.get(
+      apiConfig.REGISTRATION_PORT,
+      "api/Registration/checkIfUserExists",
+      { phoneNumber: vals.phoneNumber }
+    )
+      .then((result) => {
+        if (!result) {
+          console.log("not exists");
+          HttpClient.create(
+            apiConfig.REGISTRATION_PORT,
+            "api/Registration/CreateVerficationRequest",
+            {
+              FirstName: vals.firstName,
+              LastName: vals.lastName,
+              Email: vals.email,
+              PhoneNumber: vals.phoneNumber,
+            }
+          )
+            .then((verificationKey) => {
+              setSubmitting(false);
+              props.navigation.navigate("VerifySignup", { verificationKey });
+            })
+            .catch((e) => console.log(e));
+        } else {
+          console.log("Exists");
+          props.navigation.navigate("Auth");
+        }
+      })
+      .catch((e) => console.log(e));
+  };
+
   return (
     <View>
-      <Text>Signup</Text>
+      <SignupForm submitForm={handleSubmitForm} submitting={submitting} />
     </View>
   );
 };
