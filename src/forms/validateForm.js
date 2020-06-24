48ad896d5a2157af2394eb9f13676ff625e3c7f9
aviram5 <60235058+aviram5@users.noreply.github.com>
2020-06-25 02:55:32 +0300

    Update validateForm.js

--- src/forms/validateForm.js
+++ src/forms/validateForm.js
@@ -3,25 +3,28 @@ const validateForm = (state, from) => {
     email: /^([a-zA-Z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
     phoneNumber: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{5,7}$/im,
   };
+  const message = (inputField, errorType = "required") => {
+    return `*${inputField} is ${errorType}`;
+  };
   let errors = {};
   if (from === "login") {
-    if (!state.phoneNumber) errors.phoneNumber = "*Phone Number is required";
+    if (!state.phoneNumber) errors.phoneNumber = message("Phone Number");
     else if (state.phoneNumber && !regex.phoneNumber.test(state.phoneNumber))
-      errors.phoneNumber = "*Phone Number is invalid";
+      errors.phoneNumber = message("Phone Number", "invalid");
   } else if (from === "registraion") {
-    if (!state.firstName) errors.firstName = "*First Name is required";
-    if (!state.lastName) errors.lastName = "*Last Name is required";
-    if (!state.email) errors.email = "*Email address is required";
+    if (!state.firstName) errors.firstName = message("First Name");
+    if (!state.lastName) errors.lastName = message("Last Name");
+    if (!state.email) errors.email = message("Email address");
     else if (state.email && !regex.email.test(state.email))
-      errors.email = "*Email address is invalid";
-    if (!state.phoneNumber) errors.phoneNumber = "*Phone Number is required";
+      errors.email = message("Email address", "invalid");
+    if (!state.phoneNumber) errors.phoneNumber = message("Phone Number");
     else if (state.phoneNumber && !regex.phoneNumber.test(state.phoneNumber))
-      errors.phoneNumber = "*Phone Number is invalid";
+      errors.phoneNumber = message("Phone Number", "invalid");
   } else if (from === "verifyAuth") {
     if (!state.VerificationCode)
-      errors.VerificationCode = "*Verification Code is required";
+      errors.VerificationCode = message("Verification Code");
     else if (state.VerificationCode && state.VerificationCode.length < 5)
-      errors.VerificationCode = "*Verification Code is invalid";
+      errors.VerificationCode = message("Verification Code", "invalid");
   }
 
   return errors;
