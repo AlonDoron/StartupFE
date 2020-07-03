c6d95941e72c73cbaf046e64b8ad4f59ec29a050
AlonDoron <alon1212112@gmail.com>
2020-07-03 12:38:26 +0300

    Refactored store source code files and removed previous implementation

--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from "redux";
-import combineReducers from "../reducers";
-
-const rootReducer = combineReducers;
-
-const configureStore = () => createStore(rootReducer);
-
-export default configureStore;
