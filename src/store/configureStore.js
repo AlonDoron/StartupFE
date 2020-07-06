0064cf5b45370e84382d42a358118b391e4cfee3
AlonDoron <alon1212112@gmail.com>
2020-07-07 00:01:26 +0300

    Removed the usage of redux devtools for now

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "../reducers";
 
 const configureStore = () => {
-  return createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+  return createStore(rootReducer, applyMiddleware(thunk));
 };
 
 export default configureStore;
