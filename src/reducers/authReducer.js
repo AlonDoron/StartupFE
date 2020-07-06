f466b3daedfeb9005d3b06c3824b83b6651946bc
AlonDoron <alon1212112@gmail.com>
2020-07-07 00:09:16 +0300

    Removed uneccessary reducer functions and created new reducer for DONE_FETCHING string

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,45 +1,18 @@
-import {
-  USER_LOADING,
-  IS_USER_EXISTS,
-  LOGIN_SUCCESS,
-  LOGIN_FAIL,
-  REGISTRAION_SUCCESS,
-  REGISTRAION_FAIL,
-  LOGOUT_SUCCESS,
-} from "../actions/authTypes";
+import * as authTypes from "../actions/authTypes";
 import { initialState } from "./initialState";
 
 const authReducer = (state = initialState, action) => {
   switch (action.type) {
-    case USER_LOADING:
+    case authTypes.DONE_FETCHING:
       return {
         ...state,
-        isLoading: true,
+        isDoneFetching: true,
       };
 
-    case IS_USER_EXISTS:
+    case authTypes.IS_USER_EXISTS:
       return {
         ...state,
         isUserExists: action.payload,
-      };
-
-    case REGISTRAION_SUCCESS:
-      return {
-        ...state,
-        isLogin: true,
-        isLoading: false,
-        user: action.payload,
-      };
-
-    case LOGIN_FAIL:
-    case REGISTRAION_FAIL:
-    case LOGOUT_SUCCESS:
-      return {
-        ...state,
-        deviceToken: null,
-        isLogin: false,
-        isLoading: false,
-        user: {},
       };
 
     default:
