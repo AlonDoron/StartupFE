66e41795f5351ed02fd49a5e703c776c4da88916
aviram5 <60235058+aviram5@users.noreply.github.com>
2020-07-09 19:56:07 +0300

    adding Account Screen

---
+++ src/screens/app/Account.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const Account = (props) => {
+  return (
+    <View>
+      <Text>Account</Text>
+    </View>
+  );
+};
+
+export default Account;
