229feef4df94aff01e670951091b127052ff540e
AlonDoron <alon1212112@gmail.com>
2020-07-10 12:41:09 +0300

    added isUserExistsByPhoneNumber to authActions.js

--- src/actions/authAction.js
+++ src/actions/authAction.js
@@ -1,5 +1,6 @@
 import * as authTypes from "./authTypes";
-import { ApiConfig } from "../config";
+import * as authService from "../api/wrappers/authService";
+import { apiConfig } from "../config";
 import HttpClient from "../api/HttpClient";
 
 const setUserId = (userId) => {
@@ -9,23 +10,39 @@ const setUserId = (userId) => {
   };
 };
 
-const setIsUserExists = (response) => {
+export const setIsUserExists = (response) => {
   return {
     type: authTypes.SET_IS_USER_EXISTS,
     payload: response,
   };
 };
-export const isTokenExists = (token) => {
+
+export const isUserExistsByToken = (token) => {
   return (dispatch) => {
     dispatch({ type: authTypes.GET_IS_USER_EXISTS_REQUEST });
 
-    return HttpClient.get(ApiConfig.IDENTITY_PORT, "api/identity", {
+    return HttpClient.get(apiConfig.IDENTITY_PORT, "api/identity", {
       userId: token,
     })
       .then((response) => {
         dispatch({ type: authTypes.GET_IS_USER_EXISTS_SUCCESS });
         dispatch(setIsUserExists(response));
         if (response) dispatch(setUserId(token));
+      })
+      .catch((errors) => {
+        dispatch({ type: authTypes.GET_IS_USER_EXISTS_ERROR, payload: errors });
+      });
+  };
+};
+
+export const isUserExistsByPhoneNumber = (sentFrom, phoneNumber) => {
+  return (dispatch) => {
+    dispatch({ type: authTypes.GET_IS_USER_EXISTS_REQUEST });
+    return authService
+      .checkIfUserExists(sentFrom, phoneNumber)
+      .then((response) => {
+        dispatch({ type: authTypes.GET_IS_USER_EXISTS_SUCCESS });
+        dispatch(setIsUserExists(response));
       })
       .catch((errors) => {
         dispatch({ type: authTypes.GET_IS_USER_EXISTS_ERROR, payload: errors });
