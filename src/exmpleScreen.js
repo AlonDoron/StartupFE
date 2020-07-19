afed9e9aa8df657ea3c4ca45e45f69402e645f5e
aviram5 <60235058+aviram5@users.noreply.github.com>
2020-07-19 13:58:34 +0300

    Update exmpleScreen.js

--- src/exmpleScreen.js
+++ src/exmpleScreen.js
@@ -1,39 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { View } from "react-native";
-import { Button } from "react-native-paper";
+import { Button, Text } from "react-native-paper";
 import { Input } from "./components/common";
-// import useForm from "react-hook-form";
+import { useForm } from "./hooks";
+import * as yup from "yup";
 
-const ExmpleScreen = () => {
-  //   const { register, handeSubmit, errors } = useForm();
+const schema = yup.object().shape({
+  firstName: yup.string().required(),
+  lastName: yup.string().required(),
+  email: yup.string().required().email(),
+  phoneNumber: yup.string().required().min(10),
+});
+
+const ExmpleScreen = (props) => {
+  const [error, setError] = useState("");
+  const handleFormSubmit = () => {
+    schema
+      .validate({ ...values })
+      .then((valid) => {
+        console.log(valid);
+      })
+      .catch((errors) => {
+        setError(errors);
+      });
+  };
+
+  const [values, handleChange, handleSubmit] = useForm(handleFormSubmit);
+
   return (
     <View>
       <Input
         name="firstName"
+        value={values.firstName || ""}
+        onChange={(name, value) => handleChange(name, value)}
         label="First Name"
         keyboardType="default"
-        // ref={register}
       />
       <Input
         name="lastName"
+        value={values.lastName || ""}
+        onChange={(name, value) => handleChange(name, value)}
         label="Last Name"
         keyboardType="default"
-        // ref={register}
       />
       <Input
         name="email"
+        value={values.email || ""}
+        onChange={(name, value) => handleChange(name, value)}
         label="Email"
         keyboardType="email-address"
-        // ref={register}
       />
       <Input
         name="phoneNumber"
+        value={values.phoneNumber || ""}
+        onChange={(name, value) => handleChange(name, value)}
         label="Phone Number"
         keyboardType="number-pad"
-        // ref={register}
       />
 
-      <Button mode="outlined">Registration</Button>
+      <Text>{error}</Text>
+
+      <Button mode="outlined" onPress={handleSubmit}>
+        Registration
+      </Button>
     </View>
   );
 };
