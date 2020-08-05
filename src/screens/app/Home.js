f5e6ca13b6f2f32207ebe3fac7915499b5130995
aviram5 <60235058+aviram5@users.noreply.github.com>
2020-08-05 03:09:00 +0300

    Update Home.js

--- src/screens/app/Home.js
+++ src/screens/app/Home.js
@@ -4,37 +4,24 @@ import { Foundation } from "@expo/vector-icons";
 import { useLocation } from "../../hooks";
 import { withNavigationFocus } from "react-navigation";
 import { Map, SearchBar } from "../../components/common";
-import HttpClient from "../../api/HttpClient";
-import apiConfig from "../../config/apiConfig";
 import { Marker } from "react-native-maps";
+
 const Home = ({ isFocused }) => {
   const [initLocation, setInitLocation] = useState({});
   const getLocation = (location) => {
     setInitLocation(location.coords);
   };
-
   const [err] = useLocation(isFocused, getLocation);
-  useEffect(() => {
-    if (initLocation) {
-      console.log(
-        `${apiConfig.SERVER_URL}:${apiConfig.CONSUMER_PORT}/api/Consumer?Latitude=${initLocation.latitude}&Longitude=${initLocation.longitude}&Radius=1000&MeasureUnit=0`
-      );
-      HttpClient.get(
-        apiConfig.SERVER_URL,
-        apiConfig.CONSUMER_PORT,
-        `api/Consumer?Latitude=${initLocation.latitude}&Longitude=${initLocation.longitude}&Radius=1000&MeasureUnit=0`
-      )
-        .then((result) => {
-          console.log("result", result);
-        })
-        .catch((err) => console.log(err));
-    } else console.log("initLocation is undefine");
-  }, []);
 
   return (
     <View style={{ flex: 1 }}>
       <SearchBar />
-      <Map currentLocation={initLocation} />
+      <Map
+        currentLocation={{
+          latitude: 31.9884232,
+          longitude: 34.7788025,
+        }}
+      ></Map>
       {err ? <Text>Please enable location services</Text> : null}
     </View>
   );
