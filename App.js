1ffb09ba9a86749dfc2da8101d936c535104570d
AlonDoron <alon1212112@gmail.com>
2020-10-14 22:31:59 +0300

    Updated App.js to work with the new UI-Kitten provider

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ import React from "react";
 import AppNavigator from "./src/navigation/AppNavigator";
 
 import { Provider as StoreProvider } from "react-redux";
-import { Provider as PaperProvider } from "react-native-paper";
+import { ApplicationProvider, IconRegistry } from "@ui-kitten/components";
+import * as eva from "@eva-design/eva";
+import { EvaIconsPack } from "@ui-kitten/eva-icons";
 
 import configureStore from "./src/store/configureStore";
 const store = configureStore();
@@ -10,9 +12,10 @@ const store = configureStore();
 export default function App() {
   return (
     <StoreProvider store={store}>
-      <PaperProvider>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider {...eva} theme={eva.dark}>
         <AppNavigator />
-      </PaperProvider>
+      </ApplicationProvider>
     </StoreProvider>
   );
 }
