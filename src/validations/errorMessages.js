72ba8e7694215c5c08cd61b7fbd76c2ae4f608ea
AlonDoron <alon1212112@gmail.com>
2020-10-17 09:48:53 +0300

    Updated the hebrew language to handle form errors and updated errorMessages to translate errors correctly.

--- src/validations/errorMessages.js
+++ src/validations/errorMessages.js
@@ -1,5 +1,7 @@
-const errorMessages = (fieldName, errorType = "Required") => {
-  return `*${fieldName} is ${errorType} `;
+import i18n from "../i18n";
+
+const errorMessages = (fieldName, errorType = "required") => {
+  return i18n.t(`errors.${fieldName}_is_${errorType}`);
 };
 
 export default errorMessages;
