0e6ba8f000ad7c6824e3f61c65f203e19c6db363
aviram5 <60235058+aviram5@users.noreply.github.com>
2020-11-09 17:36:32 +0200

    add store item component

--- src/screens/app/myServicesScreens/ProviderStore.js
+++ src/screens/app/myServicesScreens/ProviderStore.js
@@ -1,13 +1,36 @@
 import React from "react";
 import { View } from "react-native";
 import { Text } from "@ui-kitten/components";
+import { StoreItem } from '../../../components/store'
 import i18n from '../../../i18n';
+
+let storeItems = [
+    {
+        itemName: 'bob1',
+        itemPrice: '20'
+    },
+    {
+        itemName: 'bob2',
+        itemPrice: '20'
+    },
+    {
+        itemName: 'bob3',
+        itemPrice: '20'
+    },
+]
 
 const ProviderStore = ({ navigation }) => {
     const storeName = navigation.state.params.storeName;
     return (
         <View style={{ justifyContent: "center", alignItems: "center" }}>
             <Text>{storeName}</Text>
+            {
+                storeItems.map((value, i) => {
+                    return (
+                        <StoreItem key={i} itemName={value.itemName} itemPrice={value.itemPrice} />
+                    )
+                })
+            }
         </View>
     );
 };
