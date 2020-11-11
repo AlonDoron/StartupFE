38667492bf387ea623b6d542bcae414144eebb19
AlonDoron <alon1212112@gmail.com>
2020-11-11 12:00:09 +0200

    Where the fuck is your linter aviram

--- babel.config.js
+++ babel.config.js
@@ -1,31 +1,25 @@
-const path = require('path')
+const path = require("path");
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     plugins: [
       [
         "module-resolver",
         {
-          extensions: [
-            '.js',
-            '.jsx',
-            '.android.js',
-            '.ios.js',
-            '.web.js'
-          ],
+          extensions: [".js", ".jsx", ".android.js", ".ios.js", ".web.js"],
           alias: {
-            navigation: path.resolve(__dirname, 'src/navigation'),
-            screens: path.resolve(__dirname, 'src/screens'),
-            i18n: path.resolve(__dirname, 'src/i18n'),
-            config: path.resolve(__dirname, 'src/config'),
-            components: path.resolve(__dirname, 'src/components'),
-            actions: path.resolve(__dirname, 'src/actions'),
-            api: path.resolve(__dirname, 'src/api'),
-            forms: path.resolve(__dirname, 'src/forms'),
-          }
-        }
-      ]
-    ]
+            navigation: path.resolve(__dirname, "src/navigation"),
+            screens: path.resolve(__dirname, "src/screens"),
+            i18n: path.resolve(__dirname, "src/i18n"),
+            config: path.resolve(__dirname, "src/config"),
+            components: path.resolve(__dirname, "src/components"),
+            actions: path.resolve(__dirname, "src/actions"),
+            api: path.resolve(__dirname, "src/api"),
+            forms: path.resolve(__dirname, "src/forms"),
+          },
+        },
+      ],
+    ],
   };
 };
