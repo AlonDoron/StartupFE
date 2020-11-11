e5548684cc736c6e9b43b62e54aac8ced5194385
AlonDoron <alon1212112@gmail.com>
2020-11-11 16:13:43 +0200

    Refactored providersList.js to now fetch providers

--- src/components/maps/ProvidersList.js
+++ src/components/maps/ProvidersList.js
@@ -1,20 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Marker } from "react-native-maps";
 import { View } from "react-native";
+import { locationConfig } from "config";
+import { getProviders } from "api/wrappers/appService";
+
+const getProvidersAsync = (setProviders) => {
+  getProviders({
+    Latitude: 31.978821,
+    Longitude: 34.772711,
+    Radius: 1000,
+    MeasureUnit: 0,
+  }).then((providers) => {
+    // console.log(providers.length);
+    setProviders(providers);
+  });
+};
 
 const ProvidersList = (props) => {
+  const [providers, setProviders] = useState([]);
+
+  useEffect(() => {
+    getProvidersAsync(setProviders);
+  }, []);
+
   return (
     <View>
-      {props.providers.length > 0 &&
-        props.providers.map((provider, id) => (
+      {providers.length > 0 &&
+        providers.map((provider, id) => (
           <Marker
             key={id}
             coordinate={{
-              longitude: provider.location.longitude,
-              latitude: provider.location.latitude,
+              longitude: provider.Location.Longitude,
+              latitude: provider.Location.Latitude,
             }}
-            title={provider.title}
-            description={provider.description}
+            title={provider.Title}
+            description={provider.Description}
           />
         ))}
     </View>
