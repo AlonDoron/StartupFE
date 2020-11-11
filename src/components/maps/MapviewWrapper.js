a5b11fe34a89a7a026aeb4d86a09b287fd31c20d
AlonDoron <alon1212112@gmail.com>
2020-11-11 13:42:32 +0200

    Added ProvidersList component and providersListMock to MapviewWrapper

--- src/components/maps/MapviewWrapper.js
+++ src/components/maps/MapviewWrapper.js
@@ -1,16 +1,41 @@
 import React from "react";
+import ProvidersList from "./ProvidersList";
 import MapView from "react-native-maps";
+import { Marker } from "react-native-maps";
 import { Layout, Text } from "@ui-kitten/components";
-import { StyleSheet, Dimensions } from "react-native";
+import { StyleSheet, Dimensions, View } from "react-native";
 import { mapsConfig } from "config";
 
 const MapviewWrapper = (props) => {
+  const providersListMock = [
+    {
+      id: 1,
+      location: {
+        longitude: 34.780806,
+        latitude: 32.081583,
+      },
+      title: "drug store",
+      description: "best shit in town",
+    },
+    {
+      id: 2,
+      location: {
+        longitude: 34.780806,
+        latitude: 20.081583,
+      },
+      title: "shoes store",
+      description: "great shoes!",
+    },
+  ];
+
   return (
     <Layout>
       <MapView
         style={styles.mapStyle}
         initialRegion={mapsConfig.INITIAL_REGION}
-      />
+      >
+        <ProvidersList providers={providersListMock} />
+      </MapView>
     </Layout>
   );
 };
