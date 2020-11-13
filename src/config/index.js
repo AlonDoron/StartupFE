7f618b4d6bb31481153a81be8e7ada357cf3c8e2
AlonDoron <alon1212112@gmail.com>
2020-11-13 13:53:13 +0200

    Added locationConfig.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,4 @@
 export { default as apiConfig } from "./apiConfig";
 export { default as deviceConfig } from "./deviceConfig";
 export { default as mapsConfig } from "./mapsConfig";
+export { default as locationConfig } from "./locationConfig";
---
+++ src/config/locationConfig.js
@@ -0,0 +1,11 @@
+const RADIUS = 1000;
+const MEASURE_UNIT = 0;
+const FETCH_PROVIDERS_INTERVAL_TIME = 3000;
+
+let locationConfig = {
+  RADIUS,
+  MEASURE_UNIT,
+  FETCH_PROVIDERS_INTERVAL_TIME,
+};
+
+export default locationConfig;
