b53242f4b851024717269f3aacd863401dfbfe6f
AlonDoron <alon1212112@gmail.com>
2020-11-13 13:15:48 +0200

    Added mapviewWrapper to Home.js

--- src/screens/app/Home.js
+++ src/screens/app/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import { StyleSheet } from "react-native";
 import { Text, Layout } from "@ui-kitten/components";
 import { Foundation } from "@expo/vector-icons";
+import { MapviewWrapper } from "components/maps";
 import i18n from "i18n";
 
 const Home = (props) => {
   return (
     <Layout style={styles.container}>
       <Text style={{ fontSize: 40 }}>{i18n.t("home")}</Text>
+      <MapviewWrapper />
     </Layout>
   );
 };
