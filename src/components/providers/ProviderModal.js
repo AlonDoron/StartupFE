92d79635ab4783501c14ef71c370dec2934f7adf
AlonDoron <alon1212112@gmail.com>
2020-11-15 17:44:41 +0200

    Added padding to ProviderModal.js

--- src/components/providers/ProviderModal.js
+++ src/components/providers/ProviderModal.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { View } from "react-native";
 import { Text, Button, Layout } from "@ui-kitten/components";
 import Modal from "react-native-modal";
 import i18n from "i18n";
@@ -11,26 +12,28 @@ const ProviderModal = (props) => {
       style={props.style}
       onSwipeComplete={props.onHideModal}
       onBackdropPress={props.onHideModal}
+      animationIn="zoomInDown"
       backdropOpacity={0.1}
     >
       <Layout>
-        <Text>
-          {i18n.t("firstName")}: {props.providerData.FirstName}
-        </Text>
-        <Text>
-          {i18n.t("lastName")}: {props.providerData.LastName}
-        </Text>
-        <Text>
-          {i18n.t("title")}: {props.providerData.Title}
-        </Text>
-        <Text>
-          {i18n.t("profession")}: {props.providerData.Profession}
-        </Text>
+        <View style={{ marginLeft: "2%", marginTop: "1%", marginBottom: "1%" }}>
+          <Text>
+            {i18n.t("firstName")}: {props.providerData.FirstName}
+          </Text>
+          <Text>
+            {i18n.t("lastName")}: {props.providerData.LastName}
+          </Text>
+          <Text>
+            {i18n.t("title")}: {props.providerData.Title}
+          </Text>
+          <Text>
+            {i18n.t("profession")}: {props.providerData.Profession}
+          </Text>
 
-        <Text>
-          {i18n.t("rating")}: {props.providerData.Rating}
-        </Text>
-
+          <Text>
+            {i18n.t("rating")}: {props.providerData.Rating}
+          </Text>
+        </View>
         <Button onPress={props.onHideModal}>Press To Close!</Button>
       </Layout>
     </Modal>
