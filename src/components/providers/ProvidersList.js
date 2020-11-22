4116e23ad20b2564791bd4346f24bbddcb18b237
AlonDoron <alon1212112@gmail.com>
2020-11-22 22:55:03 +0200

    Added functionallity to fetch current location and fetch providers within configured radius

--- src/components/providers/ProvidersList.js
+++ src/components/providers/ProvidersList.js
@@ -2,27 +2,33 @@ import React, { useEffect, useState } from "react";
 import { Marker } from "react-native-maps";
 import { mapsConfig, locationConfig } from "config";
 import { getProviders } from "api/wrappers/appService";
+import LocationHandler from "api/LocationHandler";
 
 const ProvidersList = (props) => {
   const [providers, setProviders] = useState([]);
 
-  const getProvidersAsync = async (setProviders) => {
+  const getProvidersAsync = async () => {
+    const location = await LocationHandler.getCurrentLocation();
+
     const locationData = {
-      Latitude: mapsConfig.INITIAL_REGION.latitude,
-      Longitude: mapsConfig.INITIAL_REGION.longitude,
+      Latitude: location.coords.latitude,
+      Longitude: location.coords.longitude,
       Radius: locationConfig.RADIUS,
       MeasureUnit: locationConfig.MEASURE_UNIT,
     };
 
-    const providers = await getProviders(locationData);
-    setProviders(providers);
+    return await getProviders(locationData);
+  };
+
+  const fetchAndSetProviders = () => {
+    getProvidersAsync().then((providers) => setProviders(providers));
   };
 
   useEffect(() => {
-    getProvidersAsync(setProviders);
+    fetchAndSetProviders();
 
     const fetchProvidersInterval = setInterval(() => {
-      getProvidersAsync(setProviders);
+      fetchAndSetProviders();
     }, locationConfig.FETCH_PROVIDERS_INTERVAL_TIME);
 
     return () => clearInterval(fetchProvidersInterval);
